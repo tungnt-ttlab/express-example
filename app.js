4e098ea754588f0412b5586ad066ba7474dacfbe
cr-tungnt <tung.tien@cybereason.com>
2024-01-22 12:01:54 +0700

    revert

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ const port = 3000;
 // const dotenv = require('dotenv')
 // dotenv.config()
 
-// mongoose.connect(process.env.MONGO_DATABASE_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
-mongoose.connect('mongodb+srv://tungnt:TIlDDHerU4gCKRwR@express-exanple.ub6hmgg.mongodb.net/test-express', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_DATABASE_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
 
 // Middleware
 app.use(bodyParser.json());
